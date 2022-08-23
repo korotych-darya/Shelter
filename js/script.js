4a96cb4d58a17aae5449fdee8722a2d07a50e701
korotych-darya <111871637+korotych-darya@users.noreply.github.com>
2022-08-23 14:19:47 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true, 
+        dots: true, 
+        adaptiveHeight:true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed:500,
+        autoplay: true,
+        autoSpeed: 1000,
+    });
+})
